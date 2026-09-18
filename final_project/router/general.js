62943bfb606b1fc78f79d014dc257867a141c3e8
maryamshuaibumusa706 <maryamshuaibumusa706@gmail.com>
2026-09-17 23:10:02 -0400

    Completed all tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,41 +3,112 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const axios = require('axios').default;
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (isValid(username)) {
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registered. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});
+    }
+  }
+  return res.status(404).json({message: "Unable to register user."});
 });
 
-// Get the book list available in the shop
+// Task 1 - Sync
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
-// Get book details based on ISBN
+// Task 2 - Sync
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn])
  });
-  
-// Get book details based on author
+
+// Task 3 - Sync
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let ans = []
+  for(const [isbn, book] of Object.entries(books)){
+    if(book.author === req.params.author){
+      ans.push(book);
+    }
+  }
+  res.send(JSON.stringify(ans,null,4));
 });
 
-// Get all books based on title
+// Task 4 - Sync
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let ans = []
+  for(const [isbn, book] of Object.entries(books)){
+    if(book.title === req.params.title){
+      ans.push(book);
+    }
+  }
+  res.send(JSON.stringify(ans,null,4));
 });
 
-//  Get book review
+// Task 5 - Sync
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn].reviews)
+});
+
+// Task 10 - Async Promise
+public_users.get('/async/books',function (req, res) {
+  let myPromise = new Promise((resolve,reject)=>{
+    resolve(books);
+  })
+  myPromise.then((result)=>{
+    res.send(JSON.stringify(result,null,4));
+  })
+});
+
+// Task 11 - Async ISBN with Promise
+public_users.get('/async/isbn/:isbn',function (req, res) {
+  const isbn = req.params.isbn;
+  let myPromise = new Promise((resolve,reject)=>{
+    if(books[isbn]){
+      resolve(books[isbn]);
+    }else{
+      reject({message:"ISBN not found"});
+    }
+  })
+  myPromise.then((result)=>{res.send(result)}).catch((err)=>{res.send(err)})
+});
+
+// Task 12 - Async Author
+public_users.get('/async/author/:author',function (req, res) {
+  const author = req.params.author;
+  let myPromise = new Promise((resolve,reject)=>{
+    let ans = []
+    for(const [isbn, book] of Object.entries(books)){
+      if(book.author === author){
+        ans.push(book);
+      }
+    }
+    resolve(ans);
+  })
+  myPromise.then((result)=>{res.send(JSON.stringify(result,null,4))})
+});
+
+// Task 13 - Async Title
+public_users.get('/async/title/:title',function (req, res) {
+  const title = req.params.title;
+  let myPromise = new Promise((resolve,reject)=>{
+    let ans = []
+    for(const [isbn, book] of Object.entries(books)){
+      if(book.title === title){
+        ans.push(book);
+      }
+    }
+    resolve(ans);
+  })
+  myPromise.then((result)=>{res.send(JSON.stringify(result,null,4))})
 });
 
 module.exports.general = public_users;
